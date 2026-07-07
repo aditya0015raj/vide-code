20b19221ccf210a1f252bbdda4bbf867a9b7e730
Aditya raj <aditya0015raj@gmail.com>
2026-07-07 21:18:35 +0530

    temporary before final

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,21 @@
+import SignInFormClient from '@/modules/auth/components/sign-in-form-client'
+import Image from 'next/image'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+    <Image
+  src="/login.svg"
+  alt="Login Image"
+  width={300}
+  height={300}
+  priority
+  className="m-6 object-cover"
+/>
+    <SignInFormClient/>
+    </>
+  )
+}
+
+export default Page

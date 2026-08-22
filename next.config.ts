d0284942203af0847ca4625bcc61cd38e05845e1
Aditya raj <aditya0015raj@gmail.com>
2026-08-22 12:56:48 +0530

    Fix cross-origin image loading

--- next.config.ts
+++ next.config.ts
@@ -24,10 +24,6 @@ const nextConfig: NextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin',
           },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
         ],
       },
     ];
